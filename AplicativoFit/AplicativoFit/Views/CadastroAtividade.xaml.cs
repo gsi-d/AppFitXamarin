b575589e3c59293884f8115e33fda23d8163db82
Guilherme <guimota_silver@hotmail.com>
2022-01-28 13:55:31 -0300

    Criando as tabs e a Shell

---
+++ AplicativoFit/AplicativoFit/Views/CadastroAtividade.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AplicativoFit.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CadastroAtividade : ContentPage
+    {
+        public CadastroAtividade()
+        {
+            InitializeComponent();
+        }
+    }
+}
