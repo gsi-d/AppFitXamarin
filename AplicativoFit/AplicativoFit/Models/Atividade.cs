b124f56869475ccacfe9dd2c369e722a9b824d09
Guilherme <guimota_silver@hotmail.com>
2022-01-31 11:11:26 -0300

    inicializando conexao com SQLite

--- AplicativoFit/AplicativoFit/Models/Atividade.cs
+++ AplicativoFit/AplicativoFit/Models/Atividade.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SQLite;
 
 namespace AplicativoFit.Models
 {
-    class Atividade
+    public class Atividade
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public DateTime Data { get; set; }
+        public double? Peso { get; set; }
+        public string Observacoes { get; set; }
     }
 }
